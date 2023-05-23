b43cdebdf3fc0f6103a86e1eae4860dabc2edb47
Nathanael Stevenson <113368837+natestevenson@users.noreply.github.com>
2023-05-23 14:09:32 -0500

    HDFS Replication

---
+++ p3 - HDFS/main.sh
@@ -0,0 +1,5 @@
+hdfs namenode -format -force
+hdfs namenode -D dfs.namenode.stale.datanode.interval=10000 -D dfs.namenode.heartbeat.recheck-interval=30000 -D dfs.webhdfs.enabled=true -fs hdfs://main:9000 &
+python3 -m jupyterlab --no-browser --ip=0.0.0.0 --port=5000 --allow-root --NotebookApp.token=''
+
+
---
+++ p3 - HDFS/worker.sh
@@ -0,0 +1,2 @@
+hdfs datanode -D dfs.datanode.data.dir=/var/datanode -fs hdfs://main:9000
+echo DEBUG
